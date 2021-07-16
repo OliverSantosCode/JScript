b8b51f027fbd8c1fd439f829f50589fccdc4130b
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-07-16 16:47:12 -0300

    added: ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,38 @@
+// # Drive
+var ages = (min,max) => Math.round(Math.random() * (max - min) + min)
+var ages = ages(10,70)
+
+if(ages >= 18) {
+    console.log(`Ages ${ages}: Qualify for driving`)
+}
+
+if(ages < 18) {
+    console.log(`Ages ${ages}: Does not qualify for driving`)
+}
+
+console.log("____________________________")
+
+if(ages >= 18) {
+    console.log(`Ages ${ages}: Qualify for driving`)
+} else {
+    console.log(`Ages ${ages}: Does not qualify for driving`)
+}
+
+console.log("____________________________")
+
+// # Books
+var books = ["maths", "programming", "economics", "history"]
+var type = Math.floor(Math.random() * books.length)
+var book = books[type]
+
+if(books == "history") {
+    console.log("History Book")
+} else if(books == "maths") {
+    console.log("Maths Book")
+} else if(books == "economics") {
+    console.log("Economics Books")
+} else {
+    console.log("Unknown Book")
+}
+
+console.log("____________________________")
