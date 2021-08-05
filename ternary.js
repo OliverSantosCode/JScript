5ccc0c9871d2651ca669fa0a254ea77518a3a559
Rodrigo Oliveira <oliversantoscode@gmail.com>
2021-08-05 20:36:36 -0300

    modified: file - ternary

--- ternary.js
+++ ternary.js
@@ -5,3 +5,17 @@ var age = age(10, 20);
 age >= 18
 	? console.log(`${age} Qualifies for driving.`)
 	: console.log(`${age} Does not qualify for driving.`);
+
+// # Books
+var books = ["maths", "programming", "economics", "history"];
+var type = Math.floor(Math.random() * books.length);
+var book = books[type];
+
+console.log("________________________________");
+book == "history"
+	? console.log("History Book")
+	: book == "maths"
+	? console.log("Maths Book")
+	: book == "economics"
+	? console.log("Economics Book")
+	: console.log("Unknown Book");
