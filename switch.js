49638385aadb4e7d2f2a9f05fbb09fcf3f4f81fd
OliverSantosCode <oliversantoscode@gmail.com>
2021-10-13 17:57:22 -0300

    modified: file - switch

--- switch.js
+++ switch.js
@@ -1,9 +1,12 @@
 // # Books
-var books = ["Java", "HTML", "CSS3", "Javascript"]
+var books = ["Java", "HTML", "CSS3", "Javascript", "Python"]
 var type = Math.round(Math.random() * books.length);
 var book = books[type];
 
 switch (book) {
+    case "Python":
+        console.log(`\t${book} Book`);
+        break;
     case "HTML":
         console.log(`\t${book} Book`);
         break;
