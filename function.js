58fc5e4f94608a2574026199a1995022d3910287
OliverSantosCode <oliversantoscode@gmail.com>
2021-10-15 19:32:48 -0300

    modified: file - function

--- function.js
+++ function.js
@@ -7,4 +7,8 @@ function sum(num1, num2) {
 function sub(num1, num2) {
 	return num1 - num2;
 }
-console.log(`Result sum: ${sum(5, 5)}, Result sub: ${sub(10, 3)}`);
+
+function mult(num1, num2) {
+	return num1 * num2;
+}
+console.log(`Result sum: ${sum(5, 5)}, Result sub: ${sub(10, 3)}, Result mult: ${mult(2,5)}`);
