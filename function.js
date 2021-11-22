ebbde95adfb4c61b302f3b54491cedc0c03d906e
OliverSantosCode <oliversantoscode@gmail.com>
2021-11-22 17:39:43 -0300

    modified: file - function

--- function.js
+++ function.js
@@ -19,8 +19,14 @@ function div(num1, num2) {
 function rest(num1, num2) {
 	return num1 % num2;
 }
+
+function pair(num) {
+	return num % 2;
+}
+
 console.log(`Result sum: ${sum(5, 5)},
  Result sub: ${sub(10, 3)}, 
  Result mult: ${mult(2,5)}, 
- Result div: ${div(10,5)}
- Result rest ${rest(3,2)}`);
+ Result div: ${div(10,5)},
+ Result rest ${rest(3,2)},
+ Result pair ${pair(4)}`);
